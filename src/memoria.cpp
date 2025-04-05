504804436cfba5c260c2cd54a6b2add156241279
MauricioCasillas <a24310227@live.ceti.mx>
2025-04-05 01:25:53 +0000

    Practica 3

--- src/memoria.cpp
+++ src/memoria.cpp
@@ -5,26 +5,34 @@ private:
     bool size;
     bool encendido;
     bool otro;
+public:
+    void Inicializar(){
+        size = 1;
+        encendido = true;
+        otro = true;
+    }
 };
 
 int main(int argc, char const *argv[])
 {
    
-    cout<<"---------------------------------"<<endl;
+    cout<<endl<<"---------------------------------"<<endl;
     cout<<"Tamaño de memoria"<<endl;
+    cout<<"---------------------------------"<<endl;
     cout<<"Tamaño int: "<<sizeof(int)<<endl;
     cout<<"Tamaño char: "<<sizeof(char)<<endl;
     cout<<"Tamaño bool: "<<sizeof(bool)<<endl;
     cout<<"Tamaño float:"<<sizeof(float)<<endl;
+    cout<<"Tamaño void: "<<sizeof(void)<<endl;
     cout<<"Tamaño double: "<<sizeof(double)<<endl<<endl;
 
     // 8 375 186 227
 
-    cout<<"---------------------------------"<<endl;
-
     int a = 27;
     int b = 50;
+    cout<<endl<<"---------------------------------"<<endl;
     cout<<"Memoria Estatica: "<<endl;
+    cout<<"---------------------------------"<<endl;
     cout<<"Direccion a: "<<&a<<endl;
     cout<<"Valor     a: "<<a<<endl;
     cout<<"Tamaño    a: "<<sizeof(a)<<endl;
@@ -36,16 +44,18 @@ int main(int argc, char const *argv[])
     // * = Operador de indireccion
     // & = Operador de direccion
 
-    cout<<"---------------------------------"<<endl;
+    cout<<endl<<"---------------------------------"<<endl;
     cout<<"Direcciones de memoria: "<<endl;
+    cout<<"---------------------------------"<<endl;
     int* direccionA = &a;
     cout<<"Valor de DireccionA: "<<direccionA<<endl;
     cout<<"Tamaño puntero: "<<sizeof(int*)<<endl;
     cout<<"Direccion (&) de direccionA: "<<&direccionA<<endl;
     cout<<"Indireccion (*) de direccionA: "<<*direccionA<<endl;
     
-    cout<<"---------------------------------"<<endl;
+    cout<<endl<<"---------------------------------"<<endl;
     cout<<"Arreglos: "<<endl;
+    cout<<"---------------------------------"<<endl;
     int arreglo[10];
     cout<<"Direccion de arreglo: "<<&arreglo<<endl;
     cout<<"Direccion de arreglo[0]: "<<&arreglo[0]<<endl;
@@ -53,11 +63,45 @@ int main(int argc, char const *argv[])
     cout<<"Direccion de arreglo + 0: "<<&arreglo + 0<<endl;
     cout<<"Direccion de arreglo + 1: "<<&arreglo + 1<<endl;
 
-    cout<<"---------------------------------"<<endl;
+    cout<<endl<<"---------------------------------"<<endl;
     cout<<"Objetos :"<<endl;
+    cout<<"---------------------------------"<<endl;
     cout<<"Tamaño Chorizo: "<<sizeof(Chorizo)<<endl;
     Chorizo pruebas;
     cout<<"Direccion Chorizo "<<&pruebas<<endl;
+    
+    cout<<endl<<"---------------------------------"<<endl;
+    cout<<"Memoria Dinamica C (Estructurada): "<<endl;
+    cout<<"---------------------------------"<<endl;
+    cout<<"Malloc: "<<malloc(sizeof(int))<<endl;
+    // Guardar direccion libre en un puntero
+    void* memoriaDinamica = malloc((sizeof(int)));
+   
+    // Casteo, convertir una variable en otra variable
+    cout<<"---------------------------------"<<endl;
+    cout<<"Cast variables: "<<endl;
+    int* punteroEntero = (int*)malloc(sizeof(int)); //Convertir de malloc void a entero
+    *punteroEntero = 5; //Guardar los datos a donde esta apuntando
+    cout<<"Direccion: "<<punteroEntero<<endl; //Imprimir la direccion que guardo el puntero
+    cout<<"Valor: "<<*punteroEntero<<endl; //Imprimir el valor de la direccion
+    
+    //Liberar memoria
+    free(punteroEntero); //free(memoria que quiero liberar)
+
+    cout<<endl<<"---------------------------------"<<endl;
+    cout<<"Memoria Dinamica C++ (Objetos): "<<endl;
+    cout<<"---------------------------------"<<endl;
+    cout<<"C: "<<(Chorizo*)malloc(sizeof(Chorizo))<<endl;
+    cout<<"C++: "<<new Chorizo()<<endl;
+
+    // C
+    Chorizo* punteroC = (Chorizo*)malloc(sizeof(Chorizo));
+    (*punteroC).Inicializar();
+    free(punteroC);
+
+    // C++
+    Chorizo* punteroCXX = new Chorizo();
+    delete punteroCXX;
 
     return 0;
 }
